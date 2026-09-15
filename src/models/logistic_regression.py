45d2ed38f35882165f6d3c8181e97bd384b1bde9
salmazeyada75 <salmazeyada84@gmail.com>
2026-09-15 15:04:17 +0300

    Logistic Regression

--- src/models/logistic_regression.py
+++ src/models/logistic_regression.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
+
 from src.models.base_model import BaseXGModel
 
 
@@ -15,21 +16,18 @@ class LogisticRegressionModel(BaseXGModel):
     """Interpretable baseline. See BaseXGModel for train()/predict_proba()."""
 
     def build_pipeline(self): # this method returns a pipeline that includes the scaler and the Logistic Regression classifier
-        """Build the scaler + Logistic Regression pipeline.
+        pipeline_lr=Pipeline([
+            ('scaler', StandardScaler()),
+            ('clf', LogisticRegression(max_iter=1000 , class_weight=None, random_state=42))
+        ])
+        return pipeline_lr
 
-        TODO: confirm whether any additional preprocessing steps belong here.
-        """
         
 
     def get_param_grid(self):
-        """Return the hyperparameter grid for GridSearchCV.
-
-        TODO: fill in the values to search over, e.g.
-            'clf__C': [...],
-            'clf__penalty': [...],
-            'clf__solver': [...],
-        """
-        param_grid = {
-            # TODO
-        }
-        return param_grid
+        param_grid_lr={
+            'clf__C': [0.001, 0.01 , 0.1 , 1 , 10 , 100],
+            'clf__penalty': ['l2'],
+            'clf__solver': ['lbfgs']
+         }
+        return param_grid_lr
