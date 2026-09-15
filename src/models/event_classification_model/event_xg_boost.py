da5c29be967dad08423eb46fc312ce0b287ce56a
Salma <salmass000876@gmail.com>
2026-09-16 01:03:16 +0300

    implement XGBoost model for event classification

--- src/models/event_classification_model/event_xg_boost.py
+++ src/models/event_classification_model/event_xg_boost.py
@@ -1,28 +1,29 @@
 from src.models.event_classification_model.event_base_model import EventClassificationBaseModel
+from sklearn.pipeline import Pipeline
+from xgboost import XGBClassifier
+from sklearn.utils.class_weight import compute_sample_weight
 
 
 class XGBoostEventModel(EventClassificationBaseModel):
-    """See EventClassificationBaseModel for train()/predict_proba()/evaluate()."""
-
     def build_pipeline(self):
-        # TODO: XGBClassifier(objective='multi:softprob', num_class=5,
-        #       eval_metric='mlogloss', tree_method='hist', random_state=42).
-        # tree_method='hist' is what makes this tractable at 2.27M rows.
-        raise NotImplementedError
+        pipeline_xgb = Pipeline([
+            ('clf',XGBClassifier(objective='multi:softprob' , num_class=5 , eval_metric='mlogloss' , tree_method='hist' , random_state=42))
+
+        ])
+        return pipeline_xgb
 
     def get_param_grid(self):
-        # TODO: per the guide:
-        #   'clf__n_estimators': [200, 300, 400]
-        #   'clf__max_depth': [4, 5, 6, 8]
-        #   'clf__learning_rate': [0.01, 0.05, 0.1]
-        #   'clf__subsample': [0.7, 0.8, 1.0]
-        #   'clf__colsample_bytree': [0.7, 0.8, 1.0]
-        #   'clf__min_child_weight': [1, 5, 10]
-        raise NotImplementedError
+        param_xgb={
+            'clf__n_estimators': [200, 300, 400],
+            'clf__max_depth': [4, 5, 6, 8],
+            'clf__learning_rate': [0.01, 0.05, 0.1],
+            'clf__subsample': [0.7, 0.8, 1.0],
+            'clf__colsample_bytree': [0.7, 0.8, 1.0],
+            'clf__min_child_weight': [1, 5, 10]
+        }
+        return param_xgb
 
     def get_fit_params(self, X_train, y_train) -> dict:
-        # TODO: XGBClassifier has no multiclass class_weight, so imbalance
-        # correction goes through sample_weight instead:
-        #   weights = compute_sample_weight('balanced', y_train)
-        #   return {'clf__sample_weight': weights}
-        raise NotImplementedError
+         weights = compute_sample_weight('balanced',y_train)
+         return {'clf__sample_weight':weights}
+        
